b0f61d7c0a329eeadec951b344bb28fe421b03a8
dmitrii <dmitrii.golikov91@gmail.com>
2021-01-05 17:52:10 +0200

    inital commit

---
+++ src/test/java/ee/pacyorky/gameserver/gameserver/GameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pacyorky.gameserver.gameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2fa26db49b7358f7730131a78bc7f5188e7ca48c
dmitrii <dmitrii.golikov91@gmail.com>
2021-01-08 18:16:56 +0200

    cards finished

--- src/main/java/ee/pacyorky/gameserver/gameserver/repositories/PlayerRepository.java
+++ src/main/java/ee/pacyorky/gameserver/gameserver/repositories/PlayerRepository.java
@@ -2,6 +2,8 @@ package ee.pacyorky.gameserver.gameserver.repositories;
 
 import ee.pacyorky.gameserver.gameserver.entities.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, String> {
 }

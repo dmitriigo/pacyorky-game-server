d3905aad898e2934ce0e7f1757f6f38f00fc502d
dmitrii <dmitrii.golikov91@gmail.com>
2021-08-20 16:33:08 +0300

    step cancel added

--- src/main/java/ee/pacyorky/gameserver/gameserver/services/impl/GeneralGameServiceImpl.java
+++ src/main/java/ee/pacyorky/gameserver/gameserver/services/impl/GeneralGameServiceImpl.java
@@ -5,12 +5,15 @@ import ee.pacyorky.gameserver.gameserver.entities.Card;
 import ee.pacyorky.gameserver.gameserver.entities.CardType;
 import ee.pacyorky.gameserver.gameserver.entities.Game;
 import ee.pacyorky.gameserver.gameserver.entities.Player;
+import ee.pacyorky.gameserver.gameserver.exceptions.GlobalException;
+import ee.pacyorky.gameserver.gameserver.exceptions.GlobalExceptionCode;
 import ee.pacyorky.gameserver.gameserver.services.EventDayService;
 import ee.pacyorky.gameserver.gameserver.services.GameManager;
 import ee.pacyorky.gameserver.gameserver.services.GeneralGameService;
 import ee.pacyorky.gameserver.gameserver.services.PlayerService;
 import ee.pacyorky.gameserver.gameserver.services.impl.GameExecutors.GameStartExecutor;
 import ee.pacyorky.gameserver.gameserver.services.impl.GameExecutors.GameStepExecutor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +26,7 @@ import java.util.concurrent.Future;
 import java.util.function.BiConsumer;
 
 @Service
+@Slf4j
 public class GeneralGameServiceImpl implements GeneralGameService {
 
     private final PlayerService playerService;
@@ -57,12 +61,19 @@ public class GeneralGameServiceImpl implements GeneralGameService {
 
     @Override
     public void nextStep(Long gameId, GameManager gameManager) {
+        if (steps.containsKey(gameId)) {
+            steps.get(gameId).cancel(true);
+        }
         var feature = executorService.submit(new GameStepExecutor(gameManager, playerService, eventDayService, gameId));
         steps.put(gameId, feature);
     }
 
     @Override
     public void setUpStart(Game savedGame, GameManager gameManager) {
+        if (games.containsKey(savedGame.getId())) {
+            log.error("Game already in queue");
+            throw new GlobalException("Game already in queue", GlobalExceptionCode.INTERNAL_SERVER_ERROR);
+        }
         var feature = executorService.submit(new GameStartExecutor(gameManager, playerService, eventDayService, savedGame.getId(), afterStartCallback()));
         games.put(savedGame.getId(), feature);
     }

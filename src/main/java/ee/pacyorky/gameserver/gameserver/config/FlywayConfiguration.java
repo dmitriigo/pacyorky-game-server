686c7aa749a20d4bb5d850d03bc4e8d3dec483ca
dmitrii <dmitrii.golikov91@gmail.com>
2021-08-21 12:17:52 +0300

    config fixes for tests

--- src/main/java/ee/pacyorky/gameserver/gameserver/config/FlywayConfiguration.java
+++ src/main/java/ee/pacyorky/gameserver/gameserver/config/FlywayConfiguration.java
@@ -4,13 +4,11 @@ package ee.pacyorky.gameserver.gameserver.config;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 
 @Configuration
-@Profile("!test")
 public class FlywayConfiguration {
 
     @Autowired

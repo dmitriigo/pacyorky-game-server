d805eb7320f86e47ed4c695f3a1ed72d34d8b8ce
dmitrii <dmitrii.golikov91@gmail.com>
2021-09-19 16:27:22 +0300

    return event day holiday card as name for event day if event day is holiday

--- src/main/java/ee/pacyorky/gameserver/gameserver/mappers/EventDayMapper.java
+++ src/main/java/ee/pacyorky/gameserver/gameserver/mappers/EventDayMapper.java
@@ -3,12 +3,26 @@ package ee.pacyorky.gameserver.gameserver.mappers;
 import ee.pacyorky.gameserver.gameserver.dtos.EventDayDTO;
 import ee.pacyorky.gameserver.gameserver.entities.game.EventDay;
 import ee.pacyorky.gameserver.gameserver.entities.game.Season;
+import ee.pacyorky.gameserver.gameserver.exceptions.GlobalException;
+import ee.pacyorky.gameserver.gameserver.exceptions.GlobalExceptionCode;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper
 public interface EventDayMapper {
 
+    @Mapping(target = "name", source = "eventDay", qualifiedByName = "eventDayName")
     EventDayDTO toDto(EventDay eventDay);
+
+
+    @Named(value = "eventDayName")
+    default String map(EventDay eventDay) {
+        if (eventDay.isHoliday() && eventDay.getHolidayCard() == null) {
+            throw new GlobalException("Day is holiday, but holiday card is null", GlobalExceptionCode.INTERNAL_SERVER_ERROR);
+        }
+        return eventDay.isHoliday() ? eventDay.getHolidayCard().getName() : eventDay.getName();
+    }
 
     default Long map(Season season) {
         return season.getId();

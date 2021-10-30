2a4613814fdd7f6d8a877e2512ca8ed56623de08
Dmitrii Golikov <dmitrii.golikov91@gmail.com>
2021-10-30 12:19:20 +0200

    fix event day script

--- src/main/resources/db/migration/V4__4_event_days.sql
+++ src/main/resources/db/migration/V4__4_event_days.sql
@@ -1,98 +1,98 @@
 -- noinspection SqlResolveForFile
 
 insert into event_day (id, holiday, name, season, desk_order)
-values (1, 0, 'day', 1, 101);
+values (1, 1, 'day', 1, 101);
 insert into event_day (id, holiday, name, season, desk_order)
-values (2, 1, 'svjatvechir', 1, 106);
+values (2, 0, 'svjatvechir', 1, 106);
 insert into event_day (id, holiday, name, season, desk_order)
-values (3, 1, 'rizdvo', 1, 107);
+values (3, 0, 'rizdvo', 1, 107);
 insert into event_day (id, holiday, name, season, desk_order)
-values (4, 1, 'malanki', 1, 113);
+values (4, 0, 'malanki', 1, 113);
 insert into event_day (id, holiday, name, season, desk_order)
-values (5, 1, 'vasilja', 1, 114);
+values (5, 0, 'vasilja', 1, 114);
 insert into event_day (id, holiday, name, season, desk_order)
-values (6, 1, 'golodna_kutja', 1, 118);
+values (6, 0, 'golodna_kutja', 1, 118);
 insert into event_day (id, holiday, name, season, desk_order)
-values (7, 1, 'vodohresha', 1, 119);
+values (7, 0, 'vodohresha', 1, 119);
 insert into event_day (id, holiday, name, season, desk_order)
-values (8, 0, 'day', 1, 201);
+values (8, 1, 'day', 1, 201);
 insert into event_day (id, holiday, name, season, desk_order)
-values (9, 1, 'stritennja', 1, 215);
+values (9, 0, 'stritennja', 1, 215);
 insert into event_day (id, holiday, name, season, desk_order)
-values (10, 1, 'maslenica', 1, 216);
+values (10, 0, 'maslenica', 1, 216);
 insert into event_day (id, holiday, name, season, desk_order)
-values (11, 0, 'day', 1, 217);
+values (11, 1, 'day', 1, 217);
 insert into event_day (id, holiday, name, season, desk_order)
-values (12, 0, 'day', 2, 301);
+values (12, 1, 'day', 2, 301);
 insert into event_day (id, holiday, name, season, desk_order)
-values (13, 1, 'javdohi', 2, 314);
+values (13, 0, 'javdohi', 2, 314);
 insert into event_day (id, holiday, name, season, desk_order)
-values (14, 1, 'sorok_svjatih', 2, 322);
+values (14, 0, 'sorok_svjatih', 2, 322);
 insert into event_day (id, holiday, name, season, desk_order)
-values (15, 1, 'oleksi', 2, 330);
+values (15, 0, 'oleksi', 2, 330);
 insert into event_day (id, holiday, name, season, desk_order)
-values (16, 1, 'blagoveshinnja', 2, 407);
+values (16, 0, 'blagoveshinnja', 2, 407);
 insert into event_day (id, holiday, name, season, desk_order)
-values (17, 1, 'verbna_nedelja', 2, 408);
+values (17, 0, 'verbna_nedelja', 2, 408);
 insert into event_day (id, holiday, name, season, desk_order)
-values (18, 1, 'velikden', 2, 409);
+values (18, 0, 'velikden', 2, 409);
 insert into event_day (id, holiday, name, season, desk_order)
-values (19, 0, 'day', 2, 410);
+values (19, 1, 'day', 2, 410);
 insert into event_day (id, holiday, name, season, desk_order)
-values (20, 1, 'ljalja', 2, 505);
+values (20, 0, 'ljalja', 2, 505);
 insert into event_day (id, holiday, name, season, desk_order)
-values (21, 1, 'jurija', 2, 506);
+values (21, 0, 'jurija', 2, 506);
 insert into event_day (id, holiday, name, season, desk_order)
-values (22, 1, 'mikolaja_leto', 2, 522);
+values (22, 0, 'mikolaja_leto', 2, 522);
 insert into event_day (id, holiday, name, season, desk_order)
-values (23, 1, 'trijcja', 2, 523);
+values (23, 0, 'trijcja', 2, 523);
 insert into event_day (id, holiday, name, season, desk_order)
-values (24, 0, 'day', 3, 601);
+values (24, 1, 'day', 3, 601);
 insert into event_day (id, holiday, name, season, desk_order)
-values (25, 0, 'day', 3, 602);
+values (25, 1, 'day', 3, 602);
 insert into event_day (id, holiday, name, season, desk_order)
-values (26, 0, 'day', 3, 603);
+values (26, 1, 'day', 3, 603);
 insert into event_day (id, holiday, name, season, desk_order)
-values (27, 0, 'day', 3, 604);
+values (27, 1, 'day', 3, 604);
 insert into event_day (id, holiday, name, season, desk_order)
-values (28, 0, 'day', 3, 701);
+values (28, 1, 'day', 3, 701);
 insert into event_day (id, holiday, name, season, desk_order)
-values (29, 1, 'ivana_kupala', 3, 707);
+values (29, 0, 'ivana_kupala', 3, 707);
 insert into event_day (id, holiday, name, season, desk_order)
-values (30, 1, 'petra_i_pavla', 3, 712);
+values (30, 0, 'petra_i_pavla', 3, 712);
 insert into event_day (id, holiday, name, season, desk_order)
-values (31, 1, 'illi', 3, 802);
+values (31, 0, 'illi', 3, 802);
 insert into event_day (id, holiday, name, season, desk_order)
-values (32, 1, 'makovia', 3, 814);
+values (32, 0, 'makovia', 3, 814);
 insert into event_day (id, holiday, name, season, desk_order)
-values (33, 1, 'velikii_spas', 3, 819);
+values (33, 0, 'velikii_spas', 3, 819);
 insert into event_day (id, holiday, name, season, desk_order)
-values (34, 0, 'day', 4, 901);
+values (34, 1, 'day', 4, 901);
 insert into event_day (id, holiday, name, season, desk_order)
-values (35, 1, 'vozdvizhennja_hrista', 4, 927);
+values (35, 0, 'vozdvizhennja_hrista', 4, 927);
 insert into event_day (id, holiday, name, season, desk_order)
-values (36, 0, 'day', 4, 928);
+values (36, 1, 'day', 4, 928);
 insert into event_day (id, holiday, name, season, desk_order)
-values (37, 1, 'pokrova', 4, 1014);
+values (37, 0, 'pokrova', 4, 1014);
 insert into event_day (id, holiday, name, season, desk_order)
-values (38, 0, 'day', 4, 1015);
+values (38, 1, 'day', 4, 1015);
 insert into event_day (id, holiday, name, season, desk_order)
-values (39, 0, 'day', 4, 1016);
+values (39, 1, 'day', 4, 1016);
 insert into event_day (id, holiday, name, season, desk_order)
-values (40, 0, 'day', 4, 1017);
+values (40, 1, 'day', 4, 1017);
 insert into event_day (id, holiday, name, season, desk_order)
-values (41, 0, 'day', 4, 1101);
+values (41, 1, 'day', 4, 1101);
 insert into event_day (id, holiday, name, season, desk_order)
-values (42, 1, 'mihajla', 4, 1121);
+values (42, 0, 'mihajla', 4, 1121);
 insert into event_day (id, holiday, name, season, desk_order)
-values (43, 0, 'day', 4, 1122);
+values (43, 1, 'day', 4, 1122);
 insert into event_day (id, holiday, name, season, desk_order)
-values (44, 0, 'day', 4, 1123);
+values (44, 1, 'day', 4, 1123);
 insert into event_day (id, holiday, name, season, desk_order)
-values (45, 1, 'vvedenja', 1, 1204);
+values (45, 0, 'vvedenja', 1, 1204);
 insert into event_day (id, holiday, name, season, desk_order)
-values (46, 1, 'katerini', 1, 1207);
+values (46, 0, 'katerini', 1, 1207);
 insert into event_day (id, holiday, name, season, desk_order)
-values (47, 1, 'andria', 1, 1213);
+values (47, 0, 'andria', 1, 1213);
 insert into event_day (id, holiday, name, season, desk_order)
-values (48, 1, 'mikolaja_zima', 1, 1219);
+values (48, 0, 'mikolaja_zima', 1, 1219);

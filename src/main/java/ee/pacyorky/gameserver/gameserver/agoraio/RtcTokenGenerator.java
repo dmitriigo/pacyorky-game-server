2243f2f387229dd2ab40126c7755450d50023e32
Dmitrii Golikov <Dmitrii.Golikov@leongaming.com>
2021-12-01 18:11:55 +0100

    fix agora token ttl

--- src/main/java/ee/pacyorky/gameserver/gameserver/agoraio/RtcTokenGenerator.java
+++ src/main/java/ee/pacyorky/gameserver/gameserver/agoraio/RtcTokenGenerator.java
@@ -13,7 +13,7 @@ public final class RtcTokenGenerator {
                                                    Long gameId) {
 
         // Assign appropriate access privileges to each role.
-        int privilegeTs = (int)(System.currentTimeMillis() / 1000 + 1080);
+        int privilegeTs = (int)(System.currentTimeMillis() / 1000 + 7200);
         AccessToken builder = new AccessToken(agoraProperties.getAgoraId(), agoraProperties.getAgoraCertificate(), String.valueOf(gameId), "");
         builder.addPrivilege(AccessToken.Privileges.K_JOIN_CHANNEL, privilegeTs);
         builder.addPrivilege(AccessToken.Privileges.K_PUBLISH_AUDIO_STREAM, privilegeTs);
